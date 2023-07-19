10233867ded7150a6579a920673077fad957d10e
Erin.Schneider <erinsuzy@gmail.com>
2023-07-18 21:06:13 -0500

    table edit2

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,9 +10,9 @@ const Table = ({ sat }) => {
         </tr>
         </thead>
         <tbody>
-          {sat.map((id, data) => {
+          {sat.map((data) => {
             return (
-              <tr key = {id}>
+              <tr key = {satData.id}>
                 <td>{data.name}</td>
                 <td>{data.type}</td>
                 <td>{data.launchDate}</td>
