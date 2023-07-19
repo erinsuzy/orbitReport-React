57faeb4f54b7f58f63c116f24b6d9e6d179e256c
Erin.Schneider <erinsuzy@gmail.com>
2023-07-18 20:52:25 -0500

    buttons2

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,23 +1,23 @@
 import satData from "./satData";
 
-
-
 const Buttons = (filterByType, setSat, displaySats) => {
   return (
-    <><div>
+    <div>
       {displaySats.map((sat, id) => {
         return (
+          <div>
           <button onClick={() => filterByType(sat)} key={id}>
             {sat} Orbit
           </button>
+          </div>
         );
-
+          
       })}
-    </div><div>
+    
         <button> onClick={() => setSat(satData)}
           All Orbits</button>
-      </div></>
+    </div>  
   );
-};
 
+  }
 export default Buttons;
